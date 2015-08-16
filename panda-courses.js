3c67282cae3c1c51c850867105243f39a2d4a971
Cole Townsend <cole@coletownsend.com>
2015-08-16 02:01:07 -0400

    yay

--- panda-courses.js
+++ panda-courses.js
@@ -1,4 +1,9 @@
 if (Meteor.isClient) {
+  Template.registerHelper('isSession', function(session) {
+    if (session && Session.get(session) === "subscribedSuccess") {
+      return true;
+    }
+  });
   Template.home.rendered = function() {
     var listItems = document.querySelectorAll('.glider')
     var i = 0;
@@ -32,22 +37,20 @@ if (Meteor.isClient) {
   };
 };
 
- var subscribeMessage = 'Sign up for early access';
 // You can as well pass different parameters on each call
 var mailChimp = new MailChimp( /* apiKey, { version: '2.0' } */ );
 
-mailChimp.call( 'campaigns', 'list', {
-        start: 0,
-        limit: 1000
-    },
-    // Callback beauty in action
-    function ( error, result ) {
-        if ( error ) {
-            console.error( '[MailChimp][Campaigns][List] Error: %o', error );
-        } else {
-            // Do something with your data!
-            console.info( '[MailChimp][Campaigns][List]: %o', result );
-        }
+mailChimp.call('campaigns', 'list', {
+    start: 0,
+    limit: 1000
+  },
+  // Callback beauty in action
+  function(error, result) {
+    if (error) {
+      console.error('[MailChimp][Campaigns][List] Error: %o', error);
+    } else {
+      // Do something with your data!
+      console.info('[MailChimp][Campaigns][List]: %o', result);
     }
+  }
 );
-
